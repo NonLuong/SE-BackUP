3ebb7823df2e08e1e93a62d9d08b0779728684ea
Thkawaii <xxtheeparatxx@gmail.com>
2025-01-06 19:27:38 +0700

    fix payment review

--- frontend/src/pages/payment/payment.tsx
+++ frontend/src/pages/payment/payment.tsx
@@ -240,13 +240,6 @@ const Payment: React.FC = () => {
   return (
     <div className="nn">
       <div className="payment-container1">
-        <video autoPlay muted loop className="bm">
-          <source
-            src="https://media.istockphoto.com/id/1293937827/th/%E0%B8%A7%E0%B8%B4%E0%B8%94%E0%B8%B5%E0%B9%82%E0%B8%AD/%E0%B8%82%E0%B8%B1%E0%B8%9A%E0%B8%A3%E0%B8%96%E0%B8%AD%E0%B8%B8%E0%B9%82%E0%B8%A1%E0%B8%87%E0%B8%84%E0%B9%8C%E0%B8%AE%E0%B8%AD%E0%B8%A5%E0%B9%81%E0%B8%A5%E0%B8%99%E0%B8%94%E0%B9%8C%E0%B8%88%E0%B8%B2%E0%B8%81%E0%B9%81%E0%B8%A1%E0%B8%99%E0%B8%AE%E0%B8%B1%E0%B8%95%E0%B8%95%E0%B8%B1%E0%B8%99%E0%B9%84%E0%B8%9B%E0%B9%80%E0%B8%88%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%8B%E0%B8%B5%E0%B8%A2%E0%B9%8C%E0%B8%8B%E0%B8%B4%E0%B8%95%E0%B8%B5%E0%B9%89%E0%B9%82%E0%B8%94%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%A1%E0%B8%B5%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%88%E0%B8%A3%E0%B8%B2%E0%B8%88%E0%B8%A3-%E0%B8%A1%E0%B8%B8%E0%B8%A1%E0%B8%A1%E0%B8%AD%E0%B8%87%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%9C%E0%B8%B9%E0%B9%89%E0%B8%82%E0%B8%B1%E0%B8%9A%E0%B8%82%E0%B8%B5%E0%B9%88.mp4?s=mp4-640x640-is&k=20&c=kdB31F7tkd1SQl0AybWQSweLq8tbCKL9xOO6cuK0EZA="
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
         <header className="payment-header">
           {/* <div className="sidebar">
             {menuItems.map((item) => (
