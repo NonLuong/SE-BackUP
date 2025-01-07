7a52b79d1c0ba43b686eeb1418df2b526be98743
suphutsorn <suphutsorn092003@gmail.com>
2025-01-07 08:21:11 +0700

    ิแก้ booking

--- frontend/src/pages/editprofile/editprofile.tsx
+++ frontend/src/pages/editprofile/editprofile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Card, Avatar, Button, Input, Form, message, Typography } from "antd";
 import { UserOutlined } from "@ant-design/icons";
@@ -127,4 +127,4 @@ const EditProfile: React.FC = () => {
   );
 };
 
-export default EditProfile;
+export default EditProfile;*/
