b9022674a000cf4083498e4675e79b4fa9334591
NonLuong <nontakarn1596@gmail.com>
2025-01-18 17:40:51 +0700

    Room Entity - close #53

--- backend/entity/room.go
+++ backend/entity/room.go
@@ -10,6 +10,7 @@ type Rooms struct {
 	Capacity        uint8     `json:"capacity" valid:"required~Capacity is required"`
 	CurrentBookings uint8     `json:"current_bookings"`
 	TrainerID       uint      `json:"trainer_id" valid:"required~TrainerID is required"`
-	Trainer         *Trainers `gorm:"foreignKey:TrainerID" json:"trainer"`
+	Trainer         *Trainers `gorm:"foreignKey:TrainerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"trainer"`
 	Detail          string    `json:"detail" valid:"required~Detail is required"`
+	Title           string    `json:"title" valid:"required~title is required"`
 }
