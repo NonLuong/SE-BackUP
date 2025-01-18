60084c524161af3afb0a7c536a44f4835eff06fb
suphutsorn <suphutsorn092003@gmail.com>
2025-01-18 09:53:42 +0700

    validate readstatus entity message close #127

--- backend/entity/message.go
+++ backend/entity/message.go
@@ -8,6 +8,7 @@ import (
 // Message Entity
 type Message struct {
 	gorm.Model
+
 	Content      string    `json:"content" valid:"required~Content is required."`
 	MessageType  string    `json:"message_type" valid:"required~Message Type is required."` // เช่น text, image, video
 	ReadStatus   bool      `json:"read_status" valid:"-"`
@@ -15,6 +16,7 @@ type Message struct {
 	SenderID     uint      `json:"sender_id" valid:"required~Sender ID is required."`
 	SenderType   string    `json:"sender_type" valid:"required~Sender Type is required."` // เช่น Passenger, Driver
 
+	
 	RoomID       uint      `json:"room_id" valid:"required~Room ID is required."`
 	RoomChat     RoomChat  `gorm:"foreignKey:RoomID" json:"room_chat" valid:"-"`
 
@@ -25,7 +27,7 @@ type Message struct {
 	BookingID    uint      `json:"booking_id" valid:"-"`
 	Booking      Booking   `gorm:"foreignKey:BookingID" json:"booking" valid:"-"`
 
-	
+
 	DriverID     uint      `json:"driver_id" valid:"-"`
 	Driver       Driver    `gorm:"foreignKey:DriverID" json:"driver" valid:"-"`
 }
