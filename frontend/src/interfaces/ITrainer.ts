19e4a5130f25d0ed1f2fcc91bed0469379b6d953
NonLuong <nontakarn1596@gmail.com>
2025-01-18 17:43:27 +0700

    Add Trainer - close #139

--- frontend/src/interfaces/ITrainer.ts
+++ frontend/src/interfaces/ITrainer.ts
@@ -1,21 +1,21 @@
 export interface TrainersInterface {
-    message: string;
+  message: string;
 
-    ID?: number;
-  
-    FirstName?: string;
-  
-    LastName?: string;
-  
-    Email?: string;
-  
-    Phone?: string;
-  
-    Age?: number;
-  
-    BirthDay?: string;
-  
-    GenderID?: number;
-  
-    rolesId: number;
-  }
+  ID?: number;
+
+  FirstName?: string;
+
+  LastName?: string;
+
+  Email?: string;
+
+  Phone?: string;
+
+  Age?: number;
+
+  BirthDay?: string;
+
+  GenderID?: number;
+
+  rolesId: number;
+}
