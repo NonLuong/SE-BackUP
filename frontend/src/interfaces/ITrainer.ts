1067feca61486bfee1a9934f167e4a14b1749463
NonLuong <nontakarn1596@gmail.com>
2025-01-21 00:49:30 +0700

    แก้ไข ITrainer

--- frontend/src/interfaces/ITrainer.ts
+++ frontend/src/interfaces/ITrainer.ts
@@ -2,11 +2,16 @@ export interface TrainersInterface {
   message: string;
   ID?: number;
   FirstName?: string;
+  first_name?: string;
   LastName?: string;
+  last_name?: string;
   Email?: string;
+  email?: string;
   Phone?: string;
   Age?: number;
   BirthDay?: string;
+  birthday?: string;
   GenderID?: number;
+  gender_id?: number;
   rolesId: number;
 }
