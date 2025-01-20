e0f52d8797160c68e1ca052cc563883568979adf
NonLuong <nontakarn1596@gmail.com>
2025-01-20 23:53:43 +0700

    แก้ไข TrainerEdit

--- frontend/src/interfaces/ITrainer.ts
+++ frontend/src/interfaces/ITrainer.ts
@@ -1,21 +1,12 @@
 export interface TrainersInterface {
   message: string;
-
   ID?: number;
-
   FirstName?: string;
-
   LastName?: string;
-
   Email?: string;
-
   Phone?: string;
-
   Age?: number;
-
   BirthDay?: string;
-
   GenderID?: number;
-
   rolesId: number;
 }
